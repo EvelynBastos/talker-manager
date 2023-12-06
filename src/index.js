10ded94ac04438d5da6405734fa3daec27f3fb2c
EvelynBastos <lynbastos@icloud.com>
2023-12-06 00:49:56 -0300

    req-01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const talkerF = path.resolve(__dirname, './talker.json');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +13,13 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await fs.readFile(talkerF, 'utf-8');
+  const talkersJson = JSON.parse(talkers);
+
+  res.status(HTTP_OK_STATUS).json(talkersJson);
 });
 
 app.listen(PORT, () => {
