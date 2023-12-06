26d05aea2c6a0e916fde961b4ffb4881cbb7c960
EvelynBastos <lynbastos@icloud.com>
2023-12-06 01:05:23 -0300

    req-02

--- src/index.js
+++ src/index.js
@@ -22,6 +22,19 @@ app.get('/talker', async (req, res) => {
   res.status(HTTP_OK_STATUS).json(talkersJson);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await fs.readFile(talkerF, 'utf-8');
+  const talkersJson = JSON.parse(talkers);
+
+  const talker = talkersJson.find((talk) => talk.id === Number(req.params.id));
+
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(HTTP_OK_STATUS).json(talker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
